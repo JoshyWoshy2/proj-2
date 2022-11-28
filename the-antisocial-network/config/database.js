3a5de6c1e35b8baf9c25d67578c82276b92e4dec
JoshyWoshy2 <jandreas.ski@gmail.com>
2022-11-28 14:59:42 -0700

    db config'd

--- the-antisocial-network/config/database.js
+++ the-antisocial-network/config/database.js
@@ -1,8 +1,17 @@
 const mongoose = require("mongoose")
+require('dotenv').config()
 
-mongoose.connect(process.env.DATABASE_URL)
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true
+})
 
 const db = mongoose.connection
+
+console.log(process.env.DATABASE_URL)
+
+db.on('error', function (error){
+    console.log(error)
+})
 
 db.on('connected', function (){
     console.log(`Uplink established at ${db.host}:${db.port},
