5712793023e03efbe4bd15cc17d2ce8463e45b07
JoshyWoshy2 <jandreas.ski@gmail.com>
2022-11-29 11:20:10 -0700

    utter confusion

--- the-antisocial-network/routes/index.js
+++ the-antisocial-network/routes/index.js
@@ -1,11 +1,30 @@
 var express = require('express');
 var router = express.Router();
-const dburl = process.env.DATABASE_URL
+// const passport = require('passport')
+// const dburl = process.env.DATABASE_URL
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.redirect('/posts');
 });
+
+// router.get('/auth/google', passport.authenticate(
+//   'google',
+//   {scope: ['profile', 'email']}
+// ))
+
+// router.get('/oauth2callback', passport.authenticate(
+//   'google',
+//   {
+//     successRedirect: '/posts',
+//     failureRedirect: '/posts'
+//   }
+// ))
+
+// router.get('/logout', function(req, res){
+//   req.logout()
+//   res.redirect('/posts')
+// })
 
 module.exports = router;
