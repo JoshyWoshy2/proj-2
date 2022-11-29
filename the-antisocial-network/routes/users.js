64d0effea0352c1f9c6bc924eafbda716173f8e1
JoshyWoshy2 <jandreas.ski@gmail.com>
2022-11-29 11:19:48 -0700

    utter confusion

--- the-antisocial-network/routes/users.js
+++ the-antisocial-network/routes/users.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
-
+const passport = require('passport')
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('/users');
 });
+
+router.get('/auth/google', passport.authenticate(
+  'google',
+  {scope: ['profile', 'email']}
+))
+
+router.get('/oauth2callback', passport.authenticate(
+  'google',
+  {
+    successRedirect: '/posts',
+    failureRedirect: '/posts'
+  }
+))
+
+router.get('/logout', function(req, res){
+  req.logout()
+  res.redirect('/posts')
+})
 
 module.exports = router;
